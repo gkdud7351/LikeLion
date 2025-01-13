fe496c63ba62a08d1827a2a62f2a273d1e0b1135
gkdud7351 <gkdud7351@gmail.com>
2025-01-13 17:20:46 +0900

    '250113'

---
+++ next15-ts/app/page.tsx
@@ -0,0 +1,13 @@
+import { DfMenuService } from "@/services/DfMenuService";
+
+const Page = async () => {
+  const service = new DfMenuService();
+  const menus = await service.getList();
+
+  return (
+    <div>
+      <pre>{JSON.stringify(menus, null, 2)}</pre>
+    </div>
+  );
+};
+export default Page;
---
+++ next15-ts/services/DfMenuService.ts
@@ -0,0 +1,24 @@
+import { MenuListModel } from "@/models/MenuListModel";
+import { SbMenuRepository } from "@/repositories/SbMenuRepository";
+import { Menu } from "@/entities/Menu";
+
+export class DfMenuService {
+  // private repositpry : SbMenuRepository;
+  // constructor() {
+  //   this.repository=new SbMenuRepository()
+  // }
+  // 생성자에 보호모드 써주면 객체초기화가 자동으로 이루어진다
+  // 아래 코드와 위 주석 코드는 같은 의미
+  constructor(private repository: SbMenuRepository = new SbMenuRepository()) {}
+
+  async getList(): Promise<MenuListModel[]> {
+    const menus: Menu[] = await this.repository.fetchAll();
+    return menus.map((menu) => {
+      const model: MenuListModel = {
+        ...menu,
+        img: "default.svg",
+      };
+      return model;
+    });
+  }
+}
